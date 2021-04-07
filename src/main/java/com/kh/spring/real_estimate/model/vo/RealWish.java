e72a34bbe9f3e17185d8c98b1e85a3481306561f
baechaeeun <bchaeeun@naver.com>
2021-04-07 13:12:17 +0900

    commit

--- src/main/java/com/kh/spring/real_estimate/model/vo/RealWish.java
+++ src/main/java/com/kh/spring/real_estimate/model/vo/RealWish.java
@@ -3,38 +3,32 @@ package com.kh.spring.real_estimate.model.vo;
 public class RealWish {
 	private int stuffNo;
 	private String loginUser;
-	private String status="Y";
-	
-	
-	
+
 	public RealWish(int stuffNo, String loginUser) {
 		super();
 		this.stuffNo = stuffNo;
 		this.loginUser = loginUser;
 	}
+
 	public int getStuffNo() {
 		return stuffNo;
 	}
+
 	public void setStuffNo(int stuffNo) {
 		this.stuffNo = stuffNo;
 	}
+
 	public String getLoginUser() {
 		return loginUser;
 	}
+
 	public void setLoginUser(String loginUser) {
 		this.loginUser = loginUser;
 	}
-	
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
+
 	@Override
 	public String toString() {
-		return "RealWish [stuffNo=" + stuffNo + ", loginUser=" + loginUser + ", status=" + status + "]";
+		return "RealWish [stuffNo=" + stuffNo + ", loginUser=" + loginUser + "]";
 	}
-	
-	
+
 }
